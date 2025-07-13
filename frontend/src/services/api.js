7570e1aff9a628bc12f1bcf6e82953b677e09e43
gcartes1988 <gustavocartes@gmail.com>
2025-07-13 19:16:11 -0400

    🌐 Cambia baseURL a Render en api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAuth } from "firebase/auth";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api", // O REACT_APP_API_URL si prefieres usar variables
+  baseURL: process.env.REACT_APP_API_URL, 
 });
 
 api.interceptors.request.use(
